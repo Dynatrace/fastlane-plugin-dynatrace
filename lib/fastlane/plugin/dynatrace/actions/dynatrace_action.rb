4e5fe8ef94cd58a773702dc7a5db42c0e3a876a3
Marcel Breitenfellner <marcel.breitenfellner@dynatrace.com>
2020-11-09 14:35:37 +0100

    moved dss client handling to helper

--- lib/fastlane/plugin/dynatrace/actions/dynatrace_action.rb
+++ lib/fastlane/plugin/dynatrace/actions/dynatrace_action.rb
@@ -27,50 +27,7 @@ module Fastlane
           UI.message "BundleID: #{bundleId}"
         end
 
-        # get correct DTXDssClient
-        dynatraceDir = "dynatrace"
-        versionFile = "version"
-        dtxDssClientBin = "DTXDssClient"
-        dtxDssClientPath = "#{dynatraceDir}/#{dtxDssClientBin}"
-        if (params.all_keys.include? :dtxDssClientPath and not params[:dtxDssClientPath].nil?)
-          UI.message "DEPRECATION WARNING: DTXDssClientPath doesn't need to be specified anymore, the DTXDssClient is downloaded and updated automatically."
-          dtxDssClientPath = params[:dtxDssClientPath]
-        else
-          clientUri = URI("#{params[:server]}/api/config/v1/symfiles/dtxdss-download?Api-Token=#{params[:apitoken]}")
-          response = Net::HTTP.get_response(clientUri)
-
-          if not response.kind_of? Net::HTTPSuccess
-            raise "Can't connect to server, invalid response #{response.message} (#{response.code}) for URL: #{clientUri}"
-          end
-
-          remoteClientUrl = JSON.parse(response.body)["dssClientUrl"]
-          UI.message "Remote client URL: #{remoteClientUrl}"
-
-          if (!File.directory?(dynatraceDir))
-            Dir.mkdir(dynatraceDir) 
-          end
-
-          if (!File.exists?("#{dynatraceDir}/#{versionFile}") or
-              !File.exists?("#{dynatraceDir}/#{dtxDssClientBin}") or 
-              File.read("#{dynatraceDir}/#{versionFile}") != remoteClientUrl)
-            UI.message "Found a different remote DTXDssClient client. Updating local version."
-            File.delete("#{dynatraceDir}/#{versionFile}") if File.exist?("#{dynatraceDir}/#{versionFile}")
-            File.delete("#{dynatraceDir}/#{dtxDssClientBin}") if File.exist?("#{dynatraceDir}/#{dtxDssClientBin}")
-
-            File.write("#{dynatraceDir}/#{versionFile}", remoteClientUrl)
-
-            # get client from served archive
-            open(remoteClientUrl) do |zipped|
-              Zip::InputStream.open(zipped) do |unzipped|
-                entry = unzipped.get_next_entry
-                if (entry.name == dtxDssClientBin)
-                  IO.copy_stream(entry.get_input_stream, "#{dynatraceDir}/#{dtxDssClientBin}")
-                  FileUtils.chmod("+x", "#{dynatraceDir}/#{dtxDssClientBin}")
-                end
-              end
-            end
-          end
-        end
+        dtxDssClientPath = Helper::DynatraceHelper.get_dss_client(params)
 
         dsym_paths = []
         symbolFilesKey = "symbolsfile" #default to iOS
