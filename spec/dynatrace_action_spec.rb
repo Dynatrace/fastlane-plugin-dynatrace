4606f8477ebf55bc33aa0e08ec9a7f939e48a35f
Marcel Breitenfellner <marcel.breitenfellner@dynatrace.com>
2021-04-12 15:32:00 +0200

    ONE-57117 removed unused test

--- spec/dynatrace_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::DynatraceProcessSymbolsAction do
-  describe '#run' do
-    it 'prints a message' do
-      #expect(Fastlane::UI).to receive(:message).with("The dynatrace plugin is working!")
-
-      #Fastlane::Actions::DynatraceProcessSymbolsAction.run(nil)
-    end
-  end
-end
